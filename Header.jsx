8819ec588aed04dae6876c27dc4869cfec66491d
SANJAY KUMAR <sanjaypersonal031@gmail.com>
2026-06-04 21:43:58 +0530

    Add Header component for Todo List App

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>📝 Todo List App</h1>
+        <p>Organize your daily tasks efficiently</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
